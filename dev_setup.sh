167a80ad62fdea74e1dac818d111d44dfc13f954
chli <chli@chlideMacBook-Pro.local>
2025-11-18 11:06:42 +0800

    create init setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,7 +3,25 @@ git clone https://github.com/brepdiff/brepdiff.git
 
 cd brepdiff
 
-conda install -c conda-forge cmake gcc=12 gxx=12
+# conda install -c conda-forge cmake gcc=12 gxx=12 -y
+
+# headless server only
+conda install -c conda-forge \
+  xorg-libx11 \
+  xorg-libxrandr \
+  xorg-libxinerama \
+  xorg-libxcursor \
+  xorg-libxi \
+  xorg-libxext \
+  xorg-libxrender \
+  xorg-xf86vidmodeproto \
+  xorg-randrproto \
+  xorg-renderproto \
+  xorg-inputproto \
+  xorg-xextproto \
+  xorg-kbproto \
+  mesalib \
+  -y
 
 pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 \
   --index-url https://download.pytorch.org/whl/cu124
@@ -15,3 +33,4 @@ pip install dgl \
   -f https://data.dgl.ai/wheels/torch-2.4/cu124/repo.html
 
 pip install -r requirements.txt
+pip install -e .
---
+++ editor.sh
@@ -0,0 +1,3 @@
+cd ../brepdiff
+python scripts/visualization/editor.py \
+  --ckpt-path /home/lichanghao/chLi/Model/BrepDiff/abc/ckpts/epoch_5999-step_756000-val_loss_0.0141.ckpt
